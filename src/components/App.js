1c502fa20e20007f313fcf559b4ba1cb40c3beec
Shubham <plugshubham@gmail.com>
2019-04-28 22:37:24 +0530

    first one

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Table from './Table';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>React Tabulator Example</h1>
+        <hr />
+        <Table />
+      </div>
+    )
+  }
+}
---
+++ src/components/Table.js
@@ -0,0 +1,105 @@
+import React, { Component } from 'react'
+import { ReactTabulator,reactFormatter } from 'react-tabulator';
+import '../../node_modules/react-tabulator/lib/css/tabulator.min.css';
+
+
+class Table extends Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+         data:[
+            {
+                id:1,
+                name:"shubham",
+                address:"pres",
+                email:"jkldjlsjlkf",
+                mobile:234243
+            },
+            {
+                id:2,
+                name:"Vikas",
+                address:"pres",
+                email:"jkldjlsjlkf",
+                mobile:234243
+            },
+            {
+                id:3,
+                name:"Akash",
+                address:"pres",
+                email:"jkldjlsjlkf",
+                mobile:234243
+            }
+        
+        ],
+         columns:[
+            {
+                title:"id",
+                field:"id"
+            },
+            {
+                title:"name",
+                field:"name",
+                formatter: reactFormatter(<input type="text" onChange={(e)=>{
+                    console.log(e.target.value)
+                }} />)
+            },
+            {
+                title:"address",
+                field:"address",
+                formatter: reactFormatter(<input type="text" onChange={(e)=>{
+                    console.log(e.target.value)
+                }} />)
+            },
+            {
+                title:"email",
+                field:"email",
+                formatter: reactFormatter(<input type="text" onChange={(e)=>{
+                    console.log(e.target.value)
+                }} />)
+            },
+            {
+                formatter:"tickCross",
+                cellClick:(e,cell)=>{
+                       this.deleteRow(cell._cell.row)
+                }
+
+            }
+         ]
+      }
+      this.addRow = this.addRow.bind(this);
+      this.deleteRow = this.deleteRow.bind(this);
+    }
+    
+   addRow(){
+       this.setState({
+           data:[...this.state.data,{}]
+       })
+   } 
+
+   deleteRow(row){
+      function check(obj){
+            return row.data.id === obj.id
+       }
+      const index = this.state.data.findIndex(check);  
+      const newDataArray = this.state.data;
+      newDataArray.splice(index,1);
+      this.setState({
+          data:newDataArray
+      })
+   }
+
+  render() {
+    
+    return (
+      <div>
+        <button onClick={this.addRow}>ADD</button>  
+        <ReactTabulator 
+            data={this.state.data}
+            columns={this.state.columns}
+        />
+      </div>
+    )
+  }
+}
+
+export default Table;
